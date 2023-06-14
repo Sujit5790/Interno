2d094a8f33f328d3fab73584192cb5185fee0929
Sujit <sujitkumarpanda325@gmail.com>
2023-06-14 15:48:04 +0530

    Add new file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import './index.css'
 import Logo from './img/Logo.jpg';
@@ -7,16 +7,16 @@ import Photo1 from './img/Photo1.jpg';
 import Photo2 from './img/Photo2.jpg';
 import Content from './img/Content.jpg'
 
- 
 function App() {
   
     return (
-      <><img src={Logo} width="20px" height="20px" />
+      <><img src={Logo} width="20px" height="20px" alt="image"/>
       <h3>Interno</h3><><><><><><><><><>
         <ul style={{ textAlign: "right" }}>Home Pages Services Project Blog Contact</ul>
 
 
-      </><img src={Photo} width="1500px" height="150px" /></><h1 style={{ textAlign: "center" }}>About Us</h1>
+      </><img src={Photo} width="1500px" height="150px" alt="image"/>
+      </><h1 style={{ textAlign: "center" }}>About Us</h1>
         <p style={{ textAlign: "center" }}>Home/About Us</p>
         <section>
           <h3 style={{ textAlign: "center" }}><i>"I like interior design labelling.I don't</i></h3>
@@ -28,15 +28,15 @@ function App() {
           <p>It is a long established fact that a reader will be</p><p>distracted by the readable content of the page</p>
           <p>when looking at layout content using that</p><button>Our Concept</button></></>
         <div class="center">
-          <img src={Photo1} width="400px" height="300px" />
+          <img src={Photo1} width="400px" height="300px" alt="image"/>
         </div>
         <h1 style={{ textAlign: "right" }}>The End Result</h1>
 
         <p style={{ textAlign: "right" }}>It is a long established fact that a leader will be</p></>
 
         <p style={{ textAlign: "right" }}>distracted by the readable content of a page</p>
-        <img src={Photo2} width="400px" height="300px" />
-      </><img src={Content} width="800px" height="500px" />
+        <img src={Photo2} width="400px" height="300px" alt="image"/>
+      </><img src={Content} width="800px" height="500px" alt="image"/>
         <h1 style={{ textAlign: "center" }}>Creative Projects Let's have</h1>
         <h1 style={{ textAlign: "center" }}>a productive talk</h1>
         <label>Name</label>
@@ -44,7 +44,8 @@ function App() {
         <section>Email</section>
         <hr></hr>
       </><label> Hello I am interested in..</label><hr></hr><button>Send Now</button>
-        <img src={Logo} width="20px" height="20px" /><ul><h2>Interno</h2></ul>
+        <img src={Logo} width="20px" height="20px" alt="image"/>
+        <ul><h2>Interno</h2></ul>
 
       </></>
         
