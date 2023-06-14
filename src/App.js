034ee120e6907dbc6ad35bd22ccbff6b6088b9c1
Sujit <sujitkumarpanda325@gmail.com>
2023-06-14 15:52:59 +0530

    Add new file

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ import Content from './img/Content.jpg'
 function App() {
   
     return (
-      <><img src={Logo} width="20px" height="20px" alt="image"/>
+      <><img src={Logo} width="20px" height="20px" alt="Sujit"/>
       <h3>Interno</h3><><><><><><><><><>
         <ul style={{ textAlign: "right" }}>Home Pages Services Project Blog Contact</ul>
 
 
-      </><img src={Photo} width="1500px" height="150px" alt="image"/>
+      </><img src={Photo} width="1500px" height="150px" alt="Sujit"/>
       </><h1 style={{ textAlign: "center" }}>About Us</h1>
         <p style={{ textAlign: "center" }}>Home/About Us</p>
         <section>
@@ -28,15 +28,15 @@ function App() {
           <p>It is a long established fact that a reader will be</p><p>distracted by the readable content of the page</p>
           <p>when looking at layout content using that</p><button>Our Concept</button></></>
         <div class="center">
-          <img src={Photo1} width="400px" height="300px" alt="image"/>
+          <img src={Photo1} width="400px" height="300px" alt="Sujit"/>
         </div>
         <h1 style={{ textAlign: "right" }}>The End Result</h1>
 
         <p style={{ textAlign: "right" }}>It is a long established fact that a leader will be</p></>
 
         <p style={{ textAlign: "right" }}>distracted by the readable content of a page</p>
-        <img src={Photo2} width="400px" height="300px" alt="image"/>
-      </><img src={Content} width="800px" height="500px" alt="image"/>
+        <img src={Photo2} width="400px" height="300px" alt="Sujit"/>
+      </><img src={Content} width="800px" height="500px" alt="Sujit"/>
         <h1 style={{ textAlign: "center" }}>Creative Projects Let's have</h1>
         <h1 style={{ textAlign: "center" }}>a productive talk</h1>
         <label>Name</label>
@@ -44,7 +44,7 @@ function App() {
         <section>Email</section>
         <hr></hr>
       </><label> Hello I am interested in..</label><hr></hr><button>Send Now</button>
-        <img src={Logo} width="20px" height="20px" alt="image"/>
+        <img src={Logo} width="20px" height="20px" alt="Sujit"/>
         <ul><h2>Interno</h2></ul>
 
       </></>
